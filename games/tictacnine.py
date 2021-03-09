322950de647814b37582c9fc00473aac8277d3ab
n.wagensonner <n.wagensonner@manyminds.de>
2021-03-09 20:35:52 +0100

    not restrict gpu

--- games/tictacnine.py
+++ games/tictacnine.py
@@ -114,7 +114,7 @@ class MuZeroConfig:
         print(active_configuration);
 
         self.seed = active_configuration['seed']  # Seed for numpy, torch and the game
-        self.max_num_gpus = 1 # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
+        self.max_num_gpus = None # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
 
         ### Game
         self.observation_shape = (3, 9,9)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
